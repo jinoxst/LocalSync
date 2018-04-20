bb0d139baa282e1c35beeaa35310a633e9ee5e43
jinoxst <jinoxst@gmail.com>
2018-04-20 10:06:49 +0900

    rebase2

--- src/com/local/sync/employees/Main.java
+++ src/com/local/sync/employees/Main.java
@@ -73,6 +73,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("rebase-1");
+        System.out.println("rebase-2");
         if(args != null && args.length == 1 && args[0].trim().length() > 0){
             Main.reportId = Integer.parseInt(args[0].trim());
             logger.info("report id:" + Main.reportId);
